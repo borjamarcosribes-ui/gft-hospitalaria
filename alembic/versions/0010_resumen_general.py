f04ff8f97fe653c1d801b034cff5201c7b0b56a9
borjamarcosribes-ui <borjamarcosribes@gmail.com>
2026-05-25 14:08:25 +0200

    Fix Alembic revision id length for 0010 migration

--- alembic/versions/0010_resumen_general.py
+++ alembic/versions/0010_resumen_general.py
@@ -1,13 +1,13 @@
 """add resumen_general to clinical summary cache
 
-Revision ID: 0010_add_resumen_general_to_clinical_summary
+Revision ID: 0010_resumen_general
 Revises: 0009_gft_clinical_summary_cache
 Create Date: 2026-05-25
 """
 from alembic import op
 import sqlalchemy as sa
 
-revision = '0010_add_resumen_general_to_clinical_summary'
+revision = '0010_resumen_general'
 down_revision = '0009_gft_clinical_summary_cache'
 branch_labels = None
 depends_on = None
